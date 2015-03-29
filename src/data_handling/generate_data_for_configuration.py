19de47eae27fa344781b016d4e9f0ec1f0b9f7af
Jan Soendermann <jan.soendermann@googlemail.com>
2015-03-29 22:26:42 +0100

    Parallelise data generation

--- src/data_handling/generate_data_for_configuration.py
+++ src/data_handling/generate_data_for_configuration.py
@@ -4,6 +4,34 @@ from sklearn.cross_validation import KFold
 from itertools import product
 from utils import tic, toc
 import numpy as np
+from joblib import Parallel, delayed
+
+def _generate_data_for_config(dataset, classifier, param_names, param_values, percentage_data, kf, header):
+    X, y = dataset['X'], dataset['y']
+
+    scores = []
+
+    tic()
+    
+    for train_index, test_index in kf:
+        train_index_subset = train_index[:len(train_index)*percentage_data]
+        X_train, X_test = X[train_index_subset], X[test_index]
+        y_train, y_test = y[train_index_subset], y[test_index]
+
+        model = classifier(**dict(zip(param_names, param_values)))
+        clf = model.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        scores.append(score)
+        
+    elapsed_time = toc()
+    avg_score = round(np.mean(scores), 3)
+
+    output = [dataset['id']] + list(param_values) + [percentage_data, elapsed_time, avg_score]
+    print(' '.join(['{0}: {1:7.2f};'.format(*t) for t in zip(header, output)]))
+
+    return (param_values, percentage_data, elapsed_time, avg_score)
+
 
 def generate_data(name, classifier, parameters, n_folds=10, datasets='small'):
     params_tuples = parameters.items()
@@ -13,7 +41,7 @@ def generate_data(name, classifier, parameters, n_folds=10, datasets='small'):
     datasets = load_datasets(datasets=datasets)
 
     for dataset in datasets:
-        X, y, n_samples = dataset['X'], dataset['y'], dataset['n_samples']
+        n_samples = dataset['n_samples']
         kf = KFold(n_samples, n_folds)
 
         csvfile = open('out/data_{0}_{1}.csv'.format(name, dataset['name']), 'wb')
@@ -21,29 +49,16 @@ def generate_data(name, classifier, parameters, n_folds=10, datasets='small'):
         datawriter = writer(csvfile)
         header = ['dataset_id'] + param_names + ['percentage_data', 'time', 'score']
         datawriter.writerow(header)
-        
-        for param_values in product(*param_generators):
-            for percentage_data in [x * 1.0 / n_folds for x in range(1, n_folds + 1)]:
-                scores = []
 
-                tic()
+        all_parameter_values = product(*param_generators)
+        all_percentage_data_values = [x * 1.0 / n_folds for x in range(1, n_folds + 1)]
+        all_configs = product(all_parameter_values, all_percentage_data_values)
+
+        p = Parallel(n_jobs=-1)
+        res = p(delayed(_generate_data_for_config)(dataset, classifier, param_names, param_values, percentage_data, kf, header) for (param_values, percentage_data) in all_configs)
+
+        for param_values, percentage_data, elapsed_time, avg_score in res:
+            output = [dataset['id']] + list(param_values) + [percentage_data, elapsed_time, avg_score]
+            datawriter.writerow(output)
                 
-                for train_index, test_index in kf:
-                    train_index_subset = train_index[:len(train_index)*percentage_data]
-                    X_train, X_test = X[train_index_subset], X[test_index]
-                    y_train, y_test = y[train_index_subset], y[test_index]
-
-                    model = classifier(**dict(zip(param_names, param_values)))
-                    clf = model.fit(X_train, y_train)
-                    score = clf.score(X_test, y_test)
-
-                    scores.append(score)
-                    
-                elapsed_time = toc()
-                avg_score = round(np.mean(scores), 3)
-
-                output = [dataset['id']] + list(param_values) + [percentage_data, elapsed_time, avg_score]
-                datawriter.writerow(output)
-                
-                print(' '.join(['{0}: {1:7.2f};'.format(*t) for t in zip(header, output)]))
         csvfile.close()
