ba46a99cbe927ff3294610bc40977b7fb68d5369
Jan Soendermann <jan.soendermann@googlemail.com>
2015-05-20 23:19:08 +0100

    Work on report

---
+++ src/matlab/plot_bayesian_optimisation.m
@@ -0,0 +1,70 @@
+%% Set up kernel
+meanfunc =  @meanConst; hyp.mean = 0;
+
+covfunc = @covSEiso; 
+hyp.cov = log([1 1]);
+
+likfunc = @likGauss; hyp.lik = 0.1;
+
+
+%% Generate data
+x_max = 10;
+n = 100;
+
+x = linspace(0, x_max, n)';
+
+K = feval(covfunc, hyp.cov, x);
+K = K + 1e-6*eye(n);
+y = chol(K)'*randn(n, 1);
+
+z = x;
+%% fit & plot
+
+
+points = [x y];
+seen_points = points([5,27,40,52,63,71,80],:);
+sx = seen_points(:,1);
+sy = seen_points(:,2);
+
+
+
+hyps_opt = minimize(hyp, @gp, -100, @infExact, meanfunc, covfunc, likfunc, sx, sy);
+[~,~,m,s2] = gp(hyps_opt, @infExact, meanfunc, covfunc, likfunc, sx, sy, z);
+
+s = sqrt(s2);
+
+f = [m+2*sqrt(s2); flipdim(m-2*sqrt(s2),1)]; 
+
+clf;
+%win = figure;
+
+subplot(3,1,1);
+hold on; 
+grid on;
+fill([z; flipdim(z,1)], f, [7 7 7]/8)
+plot(z, m, 'r');
+plot(x, y, 'b--')
+plot(sx, sy, 'k+');
+
+axis([0,x_max,-2,2.3])
+hold off;
+
+fmax = max(sy);
+gamma = (m - fmax) ./ s
+pi = normcdf(gamma);
+ei = s .* (gamma .* normcdf(gamma)) + normpdf(gamma);
+
+subplot(3,1,2);
+title('Probability of improvement');
+hold on; 
+grid on;
+plot(x, pi);
+hold off;
+
+
+subplot(3,1,3);
+title('Expected improvement');
+hold on; 
+grid on;
+plot(x, ei);
+hold off;
